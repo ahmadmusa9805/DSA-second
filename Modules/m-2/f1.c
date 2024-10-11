9104c48ad5f4a844eefe5fe5347351c554c81098
Ahmad <ahmadmusa9805@gmail.com>
2024-10-11 15:01:37 +0600

    add a test program

---
+++ Modules/m-2/f1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(){
+    int a;
+     scanf("%d", &a);
+     int b = a+a;
+     printf("Hello World\n");
+     printf("a + a = %d", b);
+    //  printf("%d", b);
+    return 0;
+}
