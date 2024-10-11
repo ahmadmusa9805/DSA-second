ceb0bd10d4c19322d2e634f47873139d1fed244d
Ahmad <ahmadmusa9805@gmail.com>
2024-10-11 15:41:33 +0600

    add switch

---
+++ Modules/m-2/switch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+   
+   int n = 2;
+   
+
+   switch(n){
+    case 1:
+    printf("saturday\n");
+     break;
+    case 2:
+    printf("sunday\n");
+     break;
+    case 3:
+    printf("monday\n");
+     break;
+     default:
+     printf("murikha");
+   }
+
+
+    return 0;
+}
